e5e903a2a1ccef38ff32e404f7a506ffb4cae3fd
Ronnie Stafford <770290+ronniestafford@users.noreply.github.com>
2026-01-26 22:53:54 +0000

    Better handling for "latest" versions.

--- test/opencode/common_scenario_test.sh
+++ test/opencode/common_scenario_test.sh
@@ -9,18 +9,17 @@ check "Check expected version in scenario: ${EXPECTED_VERSION}"
 check "Check opencode location" which opencode
 
 # 3. Verify version output
+EXPECTED_VERSION=${EXPECTED_VERSION:-latest}
 INSTALLED_VERSION="$(opencode --version)"
 
 echo "EXPECTED_VERSION:" ${EXPECTED_VERSION}
 echo "INSTALLED_VERSION:" ${INSTALLED_VERSION}
 
-if [[ "${EXPECTED_VERSION}" == "latest" || -z "${EXPECTED_VERSION}" ]]; then
-    # latest
+if [ "${EXPECTED_VERSION}" = "latest" ]; then
     check "Check opencode INSTALLED_VERSION (latest) is semver" bash -c '
         echo "$1" | grep -qE "^[0-9]+\.[0-9]+\.[0-9]+$"
     ' _ "${INSTALLED_VERSION}"
 else
-    # pinned version test
     check "Check opencode INSTALLED_VERSION is EXPECTED_VERSION" bash -c '
         echo "$1" | grep -q "$2"
     ' _ "${INSTALLED_VERSION}" "${EXPECTED_VERSION}"
